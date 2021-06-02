136890579e502bc4fd43007173a7b3bf647176bb
gbwlxhd97 <gbwlxhd97@naver.com>
2021-06-02 17:38:21 +0900

    add ToDo && delete ToDo

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-function Home() {
+import ToDo from "../components/ToDo";
+import { actionCreators } from "../store";
+
+function Home({ toDos, addToDo }) {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
   }
   function onSubmit(e) {
     e.preventDefault();
+    addToDo(text);
     setText("");
   }
   return (
@@ -16,13 +20,23 @@ function Home() {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo {...toDo} key={toDo.id} />
+        ))}
+      </ul>
     </>
   );
 }
 
-function mapStateToProps(state, ownProps) {
-  console.log(state, ownProps); //ownProps는 home 컴포넌트의 props
+function mapStateToProps(state) {
+  return { toDos: state };
 }
 
-export default connect(mapStateToProps)(Home);
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
